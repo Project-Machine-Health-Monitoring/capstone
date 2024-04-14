68712900d948a43ad93237871cb59eeed80662df
AliAbdullaA00786 <a00786@aubh.edu.bh>
2024-04-15 00:16:01 +0300

    Updates

--- src/database/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ljzrkwoyewivcfhthral.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqenJrd295ZXdpdmNmaHRocmFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1NzU3NzcsImV4cCI6MjAxNjE1MTc3N30.bSMt5zZF012w-YVjnLTKJO0yAeMonr7VgserYfadoPM';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
